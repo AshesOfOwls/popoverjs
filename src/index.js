097f5f9f0621e7a1e6f1913032e97541a21e6aab
Scott Elwood <ashesofowls@gmail.com>
2017-06-25 12:51:40 -0700

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-const component = () => {
-  var element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+const main = () => {
+  alert('Works');
 };
 
-document.body.appendChild(component());
+main();
