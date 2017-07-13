cbd85f61d8959a28dfa6f9b764a55456382ecf61
Scott Elwood <ashesofowls@gmail.com>
2017-07-12 17:16:32 -0700

    Add sass loaders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './main.scss';
+
 const main = () => {
   alert('Works');
 };
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
+    }
 };
